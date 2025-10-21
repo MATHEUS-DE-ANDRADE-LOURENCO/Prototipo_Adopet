57bf9f856089ce943e5e6c19ea8f72d8d1a9389c
mathlori <math.louren@gmail.com>
2025-10-20 22:47:59 -0300

    Updates

--- adopetme-frontend/src/models/UserModel.tsx
+++
@@ -1,9 +0,0 @@
-export type AccountRole = 'ONG' | 'TUTOR';
-
-export interface UserRecord {
-    name: string;
-    email: string;
-    cpf: string;
-    password: string;
-    role: AccountRole;
-}
