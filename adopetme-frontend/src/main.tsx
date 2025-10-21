fa3720b401fca5967f20b4a2046cb1d6bdac3af2
Murillo Cardoso Ferreira <murillocardosoferreira@gmail.com>
2025-10-21 16:04:50 -0300

    a

--- adopetme-frontend/src/main.tsx
+++ adopetme-frontend/src/main.tsx
@@ -1,18 +1,20 @@
+// matheus-de-andrade-lourenco/prototipo_adopet/Prototipo_Adopet-83121f52595e1e8153395d49fca8b2dc2b85a109/adopetme-frontend/src/main.tsx (MODIFICADO)
+
 // main.tsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import { SessionProvider } from './context/SessionContext';
 import './index.css';
-import { BrowserRouter } from 'react-router-dom'; 
+// 👈 ALTERADO: Importar HashRouter em vez de BrowserRouter
+import { HashRouter } from 'react-router-dom'; 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    {/* 👈 ENVOLVA O <App /> COM O ROUTER */}
-    <BrowserRouter>
+    <HashRouter>
       <SessionProvider>
         <App />
       </SessionProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 );
